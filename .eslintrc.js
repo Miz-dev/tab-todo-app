a5ecb7f6851c567e4cea361174f07e9d48a188e0
shn <topdog.shn.01@gmail.com>
2022-02-05 23:44:43 +0900

    ➕ ESLintとPrettier導入

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: "true",
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:jsx-a11y/recommended",
+		"prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		// quotes: "error",
+		// "no-undef": "error",
+		// "react/prop-types": "off",
+		// "react/jsx-props-no-spreading": "off",
+		"no-var": "error",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
